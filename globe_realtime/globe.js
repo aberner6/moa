95fcbffd12a65e9ff40677161ef94c01a1559754
aberner6 <annelieberner@gmail.com>
2015-05-26 16:49:58 -0400

    historical data underneath realtime data

--- globe_realtime/globe.js
+++ globe_realtime/globe.js
@@ -18,13 +18,61 @@ DAT.Globe = function(container, opts) {
   
   var colorFn = opts.colorFn || function(x) {
     var c = new THREE.Color();
-    // c.setHSL( ( 0.6 - ( x * 0.5 ) ), 1.0, 0.5 );
     c.setHSL(.1, 1.0, 0.5 );
     return c;
   };
+// var colorFn = opts.colorFn || function(x) {
+//     var c = new THREE.Color();
+//     c.setHSL( ( 0.6 - ( x * 0.5 ) ), 1.0, 0.5 );
+//     return c;
+//   };
   var imgDir = opts.imgDir || '';
 
-  var Shaders = {
+  // var Shaders = {
+  //   'earth' : {
+  //     uniforms: {
+  //       'texture': { type: 't', value: null }
+  //     },
+  //     vertexShader: [
+  //       'varying vec3 vNormal;',
+  //       'varying vec2 vUv;',
+  //       'void main() {',
+  //         'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+  //         'vNormal = normalize( normalMatrix * normal );',
+  //         'vUv = uv;',
+  //       '}'
+  //     ].join('\n'),
+  //     fragmentShader: [
+  //       'uniform sampler2D texture;',
+  //       'varying vec3 vNormal;',
+  //       'varying vec2 vUv;',
+  //       'void main() {',
+  //         'vec3 diffuse = texture2D( texture, vUv ).xyz;',
+  //         'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+  //         'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+  //         'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
+  //       '}'
+  //     ].join('\n')
+  //   },
+  //   'atmosphere' : {
+  //     uniforms: {},
+  //     vertexShader: [
+  //       'varying vec3 vNormal;',
+  //       'void main() {',
+  //         'vNormal = normalize( normalMatrix * normal );',
+  //         'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+  //       '}'
+  //     ].join('\n'),
+  //     fragmentShader: [
+  //       'varying vec3 vNormal;',
+  //       'void main() {',
+  //         'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
+  //         'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+  //       '}'
+  //     ].join('\n')
+  //   }
+  // };
+var Shaders = {
     'earth' : {
       uniforms: {
         'texture': { type: 't', value: null }
