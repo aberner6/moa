c062dec527e34fb374912d611dcd56cf2e1f4d46
aberner6 <annelieberner@gmail.com>
2015-05-27 13:23:49 -0400

    added new csv of data, psd of world map

--- globe_realtime/globe.js
+++ globe_realtime/globe.js
@@ -28,50 +28,6 @@ DAT.Globe = function(container, opts) {
 //   };
   var imgDir = opts.imgDir || '';
 
-  // var Shaders = {
-  //   'earth' : {
-  //     uniforms: {
-  //       'texture': { type: 't', value: null }
-  //     },
-  //     vertexShader: [
-  //       'varying vec3 vNormal;',
-  //       'varying vec2 vUv;',
-  //       'void main() {',
-  //         'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-  //         'vNormal = normalize( normalMatrix * normal );',
-  //         'vUv = uv;',
-  //       '}'
-  //     ].join('\n'),
-  //     fragmentShader: [
-  //       'uniform sampler2D texture;',
-  //       'varying vec3 vNormal;',
-  //       'varying vec2 vUv;',
-  //       'void main() {',
-  //         'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-  //         'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-  //         'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
-  //         'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
-  //       '}'
-  //     ].join('\n')
-  //   },
-  //   'atmosphere' : {
-  //     uniforms: {},
-  //     vertexShader: [
-  //       'varying vec3 vNormal;',
-  //       'void main() {',
-  //         'vNormal = normalize( normalMatrix * normal );',
-  //         'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-  //       '}'
-  //     ].join('\n'),
-  //     fragmentShader: [
-  //       'varying vec3 vNormal;',
-  //       'void main() {',
-  //         'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-  //         'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
-  //       '}'
-  //     ].join('\n')
-  //   }
-  // };
 var Shaders = {
     'earth' : {
       uniforms: {
@@ -185,7 +141,16 @@ var Shaders = {
     mesh.scale.set( 1.1, 1.1, 1.1 );
     scene.add(mesh);
 
-    geometry = new THREE.BoxGeometry(0.75, 0.75, 1);
+// var radius = 5;
+// var segments = 32;
+
+// var circleGeometry = new THREE.CircleGeometry( radius, segments );        
+// var circle = new THREE.Mesh( circleGeometry, material );
+
+
+
+
+    geometry = new THREE.BoxGeometry(.75, .75, 1);
     geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,0,-0.5));
 
     point = new THREE.Mesh(geometry);
